2c514609ee1272e8c90be999de7facba2e58cd5e
ash <ashkernel02@gmail.com>
2025-05-30 20:15:21 -0600

    basic swagger docs idea. this took me way too long. going to work on API design and documentation next time and work on implementation from there.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # nodeguy
 An easily deployable backend for forum websites with a reasonable interface.
+
+## API
+
+You can also use our swagger reference at endpoint `/swagger/index.html`, or in [our docs subdirectory](./docs/).
+
+### `/new-thread`
+
+Instantiates a new thread for others to reply to.
+Input form:
+
--- main.go
+++ main.go
@@ -1,13 +1,38 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"flag"
 	"fmt"
-	"net/http"
-	"mime/multipart"
 	"log"
+	"mime/multipart"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+
+	"github.com/Borgerr/nodeguy/docs"
 )
+
+// @title           nodeguy
+// @version         0.1
+// @description     Backend API for a forum website.
+// @termsOfService  http://swagger.io/terms/
+
+// @contact.name   API Support
+// @contact.url    http://www.swagger.io/support
+// @contact.email  support@swagger.io
+
+// @license.name  Apache 2.0
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host      localhost:8080
+// @BasePath  /api/v1
+
+// @securityDefinitions.basic  BasicAuth
+
+// @externalDocs.description  OpenAPI
+// @externalDocs.url          https://swagger.io/resources/open-api/
 
 // TODO: do we want to support more than just JSON?
 // TODO: want to add some field for responding in some thread vs starting a new thread
@@ -20,7 +45,7 @@ type PostBody struct {
 func handleFile(c *gin.Context, file *multipart.FileHeader) {
 	log.Println(file.Filename)
 
-	c.SaveUploadedFile(file, "./tmp_files/" + file.Filename)
+	c.SaveUploadedFile(file, "./tmp_files/"+file.Filename)
 	c.String(http.StatusOK, fmt.Sprintf("'%s' uploaded!\r\n", file.Filename))
 }
 
@@ -33,6 +58,11 @@ func handleBody(c *gin.Context) {
 	}
 }
 
+// @Summary full post
+// @Schemes
+// @Description makes a post
+// @Success 200
+// @Router /post [post]
 func fullPost(c *gin.Context) {
 	file, err := c.FormFile("file")
 
@@ -47,22 +77,37 @@ func fullPost(c *gin.Context) {
 	}
 }
 
-func setupRouter() *gin.Engine {
-	router := gin.Default()
+func setupRouter(url string) *gin.Engine {
+	r := gin.Default()
 	// Set a lower memory limit for multipart forms
-	router.MaxMultipartMemory = 8 << 20		// 8 MiB
+	r.MaxMultipartMemory = 8 << 20 // 8 MiB
 
-	router.POST("/post", fullPost)
+	docs.SwaggerInfo.BasePath = "/api/v1"
+	v1 := r.Group("/api/v1")
+	{
+		eg := v1.Group("/")
+		{
+			eg.POST("/post", fullPost)
+		}
+	}
 
-	return router
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	// TODO: determine what this is used for, docs make it seem important
+	/*
+		ginSwagger.WrapHandler(swaggerFiles.Handler,
+			ginSwagger.URL(url),
+			ginSwagger.DefaultModelsExpandDepth(-1))
+	*/
+
+	return r
 }
 
 func main() {
-	wordPtr := flag.String("port", "8080", "open port for API")
+	hostPtr := flag.String("host", "localhost", "host for API")
+	portPtr := flag.String("port", "8080", "open port for API")
 	flag.Parse()
 
-	router := setupRouter()
+	router := setupRouter(fmt.Sprintf("http://%s:%s/swagger/doc.json", *hostPtr, *portPtr))
 
-	router.Run(fmt.Sprintf(":%s", *wordPtr))
+	router.Run(fmt.Sprintf(":%s", *portPtr))
 }
-
